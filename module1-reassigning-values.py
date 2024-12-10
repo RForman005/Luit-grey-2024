dbcfb7023226f0bed4c46c6d11a9809356cd0fff
RForman005 <richtechfit86@gmail.com>
2024-12-10 00:03:12 -0500

    Create module1-reassigning-values.py

---
+++ module1-reassigning-values.py
@@ -0,0 +1,14 @@
+age = 28
+print(age)
+new_age = age + 5
+print(new_age)
+age = 28
+age = age + 7 
+print(age)
+age +=7
+print(age)
+
+text = 'hocus' + 'pocus'
+print(text)
+print('hocus'*5)
+print(23+5)
