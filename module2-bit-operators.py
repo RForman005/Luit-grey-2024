38aa5c12212cffb01775628e119676f8967ecb19
RForman005 <richtechfit86@gmail.com>
2024-12-11 12:38:52 -0500

    Create module2-bit-operators.py

---
+++ module2-bit-operators.py
@@ -0,0 +1,17 @@
+#&
+#|
+#~
+#^
+#<<
+#>>
+#first_bit = 1
+#second_bit = 0
+
+#print(first_bit & second_bit)
+
+#print(first_bit | second_bit)
+
+#print(first_bit ^ second_bit)
+
+#print(~0)
+print(12 << 1)
