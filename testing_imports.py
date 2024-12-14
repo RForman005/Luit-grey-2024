7bae00b1f32384aaee67d58da06c240e80e72409
RForman005 <richtechfit86@gmail.com>
2024-12-14 18:15:59 -0500

    Update testing_imports.py

--- testing_imports.py
+++ testing_imports.py
@@ -1,5 +1,5 @@
-import random
+import random  # Importing the random module to generate random numbers
 
-number = random.randint(0, 10)
+number = random.randint(0, 10)  # Generating a random integer between 0 and 10 (inclusive)
 
-print(number)
+print(number)  # Printing the generated random number to the console
